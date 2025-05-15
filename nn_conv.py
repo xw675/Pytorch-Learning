594650eadeea972d945f1cadbb3292d168ddd104
xw675 <xuwei13218@gmail.com>
2025-05-15 15:36:42 +0800

    feat: add nn_conv module

--- nn_conv.py
+++ nn_conv.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-x = torch.tensor([[1, 2, 0, 3, 1],
+input = torch.tensor([[1, 2, 0, 3, 1],
                   [0, 1, 2, 3, 1],
                   [1, 2, 1, 0, 0],
                   [5, 2, 3, 1, 1],
@@ -12,20 +12,20 @@ kernel = torch.tensor([[1, 2, 1],
                        [0, 1, 0],
                        [2, 1, 0]])
 
-x_reshaped = torch.reshape(x, (1, 1, 5, 5))
+input_reshaped = torch.reshape(input, (1, 1, 5, 5))
 kernel_reshaped = torch.reshape(kernel, (1, 1, 3, 3))
 
-# print(x_reshaped.shape)
+# print(input_reshaped.shape)
 # print(kernel_reshaped.shape)
 
-output = F.conv2d(x_reshaped, kernel_reshaped, stride=1)
+output = F.conv2d(input_reshaped, kernel_reshaped, stride=1)
 print(output)
 
-output_2 = F.conv2d(x_reshaped, kernel_reshaped, stride=2)
+output_2 = F.conv2d(input_reshaped, kernel_reshaped, stride=2)
 print(output_2)
 
-output_3 = F.conv2d(x_reshaped, kernel_reshaped, stride=1, padding=1)
+output_3 = F.conv2d(input_reshaped, kernel_reshaped, stride=1, padding=1)
 print(output_3)
 
-output_4 = F.conv2d(x_reshaped, kernel_reshaped, stride=2, padding=1)
+output_4 = F.conv2d(input_reshaped, kernel_reshaped, stride=2, padding=1)
 print(output_4)

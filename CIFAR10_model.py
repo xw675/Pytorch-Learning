6fbc30737c6d6ebcd12b0891c139e1021da465d2
Lew Xu Wei <xuwei13218@gmail.com>
2025-05-19 23:00:41 +0800

    feat: add CIFAR10 model

---
+++ CIFAR10_model.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+
+class MyCIFAR10(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 32, 5, 1, padding="same"),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 32, 5, 1, padding="same"),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, 5, 1, padding="same"),
+            nn.MaxPool2d(2),
+            nn.Flatten(),
+            nn.Linear(64 * 4 * 4, 64),
+            nn.Linear(64, 10)
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+if __name__ == "__main__":
+    model = MyCIFAR10()
+    print(model)
+    input = torch.randn(64, 3, 32, 32)
+    output = model(input)
+    print(output.shape)

ba9afc38d2bee1cd476f14ea1ef54f50cce530ec
Lew Xu Wei <xuwei13218@gmail.com>
2025-05-20 21:55:16 +0800

    feat: update model

--- CIFAR10_model.py
+++ CIFAR10_model.py
@@ -8,7 +8,7 @@ class MyCIFAR10(nn.Module):
             nn.Conv2d(3, 32, 3, 1, padding="same"),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d(2),
+            nn.MaxPool2d(2, ceil_mode=True),
 
             nn.Conv2d(32, 32, 3, 1, padding="same"),
             nn.BatchNorm2d(32),
@@ -21,16 +21,17 @@ class MyCIFAR10(nn.Module):
             nn.MaxPool2d(2),
 
             nn.Conv2d(64, 128, 3, padding="same"),
-            nn.Conv2d(128, 256, 1),
+            nn.Conv2d(128, 256, 3),
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.AdaptiveAvgPool2d((2, 2)),
 
             nn.Flatten(),
             nn.Dropout(p=0.2),
-            nn.Linear(256 * 2 * 2, 64),
+            nn.Linear(256 * 2 * 2, 256),
             nn.ReLU(),
             nn.Dropout(p=0.3),
+            nn.Linear(256, 64),
             nn.Linear(64, 10)
         )
 

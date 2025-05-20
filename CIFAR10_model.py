e5e241f5cc1be6b200a2ee0e41dcf913a2f08a22
Lew Xu Wei <xuwei13218@gmail.com>
2025-05-20 16:08:56 +0800

    feat: update CIFAR10 model (add Block 4, change kernel size(3x3))

--- CIFAR10_model.py
+++ CIFAR10_model.py
@@ -5,24 +5,29 @@ class MyCIFAR10(nn.Module):
     def __init__(self):
         super().__init__()
         self.model = nn.Sequential(
-            nn.Conv2d(3, 32, 5, 1, padding="same"),
+            nn.Conv2d(3, 32, 3, 1, padding="same"),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2),
 
-            nn.Conv2d(32, 32, 5, 1, padding="same"),
+            nn.Conv2d(32, 32, 3, 1, padding="same"),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2),
 
-            nn.Conv2d(32, 64, 5, 1, padding="same"),
+            nn.Conv2d(32, 64, 3, 1, padding="same"),
             nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(2),
 
             nn.Flatten(),
             nn.Dropout(p=0.2),
-            nn.Linear(64 * 4 * 4, 64),
+            nn.Linear(128 * 4 * 4, 64),
             nn.ReLU(),
             nn.Dropout(p=0.3),
             nn.Linear(64, 10)
